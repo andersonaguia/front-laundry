ab74f32727de655f2894c7d8e469f26ee2148410
andersonaguia <andersonlaguiar@gmail.com>
2023-07-04 21:21:31 -0300

    criação dos temas light e dark

---
+++ src/themes/themeDark.ts
@@ -0,0 +1,15 @@
+import { CustomTheme } from "./theme.interfaces";
+
+export const themeDark: CustomTheme = {
+  colors: {
+    primary: "#20384F",
+    off: "#0E1E2D",
+    on: "#147EDF",
+  },
+  text: {
+    primary: "#FFFFFF",
+    secondary: "BDBDBD",
+    on: "#FFFFFF",
+    off: "#FFFFFF",
+  },
+};
---
+++ src/themes/themeLight.ts
@@ -0,0 +1,15 @@
+import { CustomTheme } from "./theme.interfaces";
+
+export const themeLight: CustomTheme = {
+  colors: {
+    primary: "#EEEEEE",
+    off: "#FFFFFF",
+    on: "#12283D",
+  },
+  text: {
+    primary: "#262626",
+    secondary: "787878",
+    on: "#000000",
+    off: "#FFFFFF",
+  },
+};
